1d3a932a869866c3f882c5f23bde9c66efff7c02
Makhdoom97 <makhdoom.jahania@gmail.com>
2022-05-12 17:39:40 +0000

    model added with server files

---
+++ node-app/app/models/user.model.js
@@ -0,0 +1,16 @@
+module.exports = mongoose => {
+    const User = mongoose.model(
+      "user",
+      mongoose.Schema(
+        {
+          firstName: String,
+          lastName: String,
+          email: String,
+          age:String,
+          introduction:String
+        },
+        { timestamps: true }
+      )
+    );
+    return Tutorial;
+  };
--- node-app/server.js
+++ node-app/server.js
@@ -9,6 +9,21 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
@@ -18,3 +33,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
