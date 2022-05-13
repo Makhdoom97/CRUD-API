2dfee1c448611fdbe93a2b736044ebf405ba02ba
Makhdoom97 <makhdoom.jahania@gmail.com>
2022-05-13 16:42:02 +0000

    added .env file

--- node-app/app/models/index.js
+++ node-app/app/models/index.js
@@ -1,8 +1,8 @@
-const dbConfig = require("../../../db.config");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+console.log(process.env.FOO);
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.url;
 db.users = require("./user.model")(mongoose);
 module.exports = db;
